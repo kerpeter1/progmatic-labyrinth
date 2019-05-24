4d8c98f7f5ff66ac5c8c74903ea8775df08fb929
Peti <kerpeter1@gmail.com>
2019-05-24 10:11:01 +0200

    load file

--- src/main/java/com/progmatic/labyrinthproject/LabyrinthImpl.java
+++ src/main/java/com/progmatic/labyrinthproject/LabyrinthImpl.java
@@ -1,8 +1,13 @@
 package com.progmatic.labyrinthproject;
 
+import com.progmatic.labyrinthproject.enums.CellType;
+import com.progmatic.labyrinthproject.enums.Direction;
+import com.progmatic.labyrinthproject.exceptions.CellException;
+import com.progmatic.labyrinthproject.exceptions.InvalidMoveException;
 import com.progmatic.labyrinthproject.interfaces.Labyrinth;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -11,8 +16,11 @@ import java.util.Scanner;
  */
 public class LabyrinthImpl implements Labyrinth {
 
+    //1st dim: row, 2nd dim:column
+    private CellType[][] labyrinth;
+
     public LabyrinthImpl() {
-        
+
     }
 
     @Override
@@ -22,18 +30,21 @@ public class LabyrinthImpl implements Labyrinth {
             int width = Integer.parseInt(sc.nextLine());
             int height = Integer.parseInt(sc.nextLine());
 
+            labyrinth = new CellType[height][width];
+            System.out.println("ok");
+
             for (int hh = 0; hh < height; hh++) {
                 String line = sc.nextLine();
                 for (int ww = 0; ww < width; ww++) {
                     switch (line.charAt(ww)) {
                         case 'W':
-                            
+                            labyrinth[hh][ww] = CellType.WALL;
                             break;
                         case 'E':
-                            
+                            labyrinth[hh][ww] = CellType.END;
                             break;
                         case 'S':
-                            
+                            labyrinth[hh][ww] = CellType.START;
                             break;
                     }
                 }
@@ -41,6 +52,51 @@ public class LabyrinthImpl implements Labyrinth {
         } catch (FileNotFoundException | NumberFormatException ex) {
             System.out.println(ex.toString());
         }
+    }
+
+    @Override
+    public int getWidth() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getHeight() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public CellType getCellType(Coordinate c) throws CellException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setSize(int width, int height) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setCellType(Coordinate c, CellType type) throws CellException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Coordinate getPlayerPosition() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean hasPlayerFinished() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Direction> possibleMoves() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void movePlayer(Direction direction) throws InvalidMoveException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
